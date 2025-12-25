0d0c8ced56488754d761331d6c6afab3f1b4c69a
masa101224 <masa101224@outlook.jp>
2025-12-25 23:35:37 +0900

    初コミット　プレイヤー操作

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpPower = 5f;
+    Rigidbody2D rb;
+    private bool ground;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+       float x = Input.GetAxisRaw("Horizontal");
+       rb.velocity  = new Vector2 (x* moveSpeed,rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && ground) 
+        { 
+            rb.velocity = new Vector2(rb.velocity.x,jumpPower);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Ground")
+         || col.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            ground  = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Ground")
+         || col.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            ground = false;
+        }
+    }
+}
