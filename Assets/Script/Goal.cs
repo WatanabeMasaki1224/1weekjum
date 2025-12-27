4a3eec777835f1508e149813d5bdb9883046b9ed
masa101224 <masa101224@outlook.jp>
2025-12-27 20:09:57 +0900

    いったん完成

--- Assets/Script/Goal.cs
+++ Assets/Script/Goal.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Goal : MonoBehaviour
@@ -9,6 +10,20 @@ public class Goal : MonoBehaviour
     public Text clearText;
     public Text clearTimeText;
     public GameObject fadePanel;
+    public Button backToTitleButton; 
+
+
+    private void Start()
+    {
+        // 最初は非表示
+        fadePanel.SetActive(false);
+        clearText.gameObject.SetActive(false);
+        clearTimeText.gameObject.SetActive(false);
+        backToTitleButton.gameObject.SetActive(false);
+
+        // ボタンに関数を紐付け
+        backToTitleButton.onClick.AddListener(BackToTitle);
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -18,6 +33,13 @@ public class Goal : MonoBehaviour
         clearText.gameObject.SetActive(true);
         clearTimeText.gameObject.SetActive(true);
         clearTimeText.text = timer.GetTimeString();
+        backToTitleButton.gameObject.SetActive(true);
         Time.timeScale = 0f;
+    }
+
+    private void BackToTitle()
+    {
+        Time.timeScale = 1f; // タイムスケール戻す
+        SceneManager.LoadScene("Title"); // タイトルシーン名に置き換え
     }
 }
